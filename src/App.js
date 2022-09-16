7738686243aa084890db70db0a6dbf1e37c1bd5a
Batuhan Karadeniz <batuhan.karadeniz@ug.bilkent.edu.tr>
2022-09-16 18:36:00 +0300

    countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import Clock from './Components/Clock';
 
 function App() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countDownDate = new Date('May 30, 2021 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours = Math.floor(
+        ((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60))
+      );
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock 
+      timerDays={timerDays}
+      timerHours ={timerHours} 
+      timerMinutes ={timerMinutes}
+      timerSeconds ={timerSeconds}
+      />
     </div>
   );
 }
